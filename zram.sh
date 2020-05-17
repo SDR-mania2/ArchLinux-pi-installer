1952b935e617c960bb6e89041f8ddfab9de927c5
SDR-mania2 <55984656+SDR-mania2@users.noreply.github.com>
2020-05-17 10:24:40 +0900

    Update zram.sh

--- zram.sh
+++ zram.sh
@@ -2,8 +2,14 @@
 #
 # Arch Wiki からのコピーです
 # 実行時にsudoをつける!
-# 固まったらCtrl+Alt+F2 -> pkill firefox -> poweroff
+# 固まったらCtrl+Alt+F2 -> pkill firefox -> pkill i3
+# -> pkill xinit -> poweroff
 #
+
+echo -n "Input on or off:"
+read param
+
+if [ $param = "on" ]; then
 
 # enable zram
 modprobe zram
@@ -12,8 +18,18 @@ echo 100M > /sys/block/zram0/disksize
 mkswap --label zram0 /dev/zram0
 swapon --priority 100 /dev/zram0
 
+fi
+
+
+if [ $param = "off" ]; then
+
 # disable zram
-# swapoff /dev/zram0
-# rmmod zram
+swapoff /dev/zram0
+rmmod zram
 # or reboot!
+
+fi
+
+free -h
+exit 0
 
